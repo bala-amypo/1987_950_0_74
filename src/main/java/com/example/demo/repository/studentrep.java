65ca88ef14402179d9ff6a2f9d3455d0a4b7ec00
bala-amypo <bala@amypo.in>
2025-12-17 06:29:59 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/studentrep.java
@@ -0,0 +1,7 @@
+package com.example.demo.repository;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.
+@Repository
+public interface studentrep extends JpaRepository{
+
+}
